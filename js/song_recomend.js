b17cf1478f629e0c538288155f6587f60df5b314
JuarezJavier <juarezjavier018@gmail.com>
2024-05-28 13:11:33 -0300

    Rehize el footer en todas las paginas, agrege la llamada a la api, y modi el mapa del footer a la pagina de contactos

---
+++ js/song_recomend.js
@@ -0,0 +1,76 @@
+let TituloArray = ["En la Ciudad de la Furia","The Scientist","Eleanor Rigby"];
+let ArtistaArray = ["Soda Stereo","Coldplay", "The Beatles"];
+
+let linea = document.querySelector("#Linea");
+let lineaCopia = linea.cloneNode(true);
+
+linea.remove();
+
+let couplet = document.querySelector("#Couplet");
+let coupletCopia = couplet.cloneNode(true);
+
+couplet.remove();
+
+let lyrics = document.querySelector("#Lyrics");
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+let CH = getRandomInt(3);
+
+let Titulo = TituloArray[CH];
+let Artista = ArtistaArray[CH];
+
+
+fetch("https://api.lyrics.ovh/v1/" + Artista + "/" + Titulo)
+.then (response => response.json())
+.then(data =>
+    {
+        let Letra = data.lyrics;
+
+        Letra = Letra.replaceAll("\n\n","\n");
+
+        let LetraArray = Letra.split("\n");
+
+        console.log(Letra);
+
+        document.querySelector("#Tema").innerHTML = "'" + Titulo + "' por " + Artista;
+
+        Create_Couplet(3,LetraArray);
+
+    }
+)
+.catch(error=>console.log(error));
+
+
+
+function Create_Couplet(N,LetraArray)
+{
+    var i;
+    for(let i=0;i<N;i++)
+        {
+            console.log(LetraArray);
+
+            let NewCouplet = coupletCopia.cloneNode(true);
+            lyrics.appendChild(NewCouplet);
+            let NewArray = Create_Line(LetraArray.indexOf(""),LetraArray,NewCouplet);
+            LetraArray.length = 0;         
+            LetraArray.push.apply(LetraArray, NewArray);
+        }
+    
+};
+
+function Create_Line(N, LetraArray, Couplet)
+{
+    var i;
+    for(i=0;i<N;i++)
+        {
+            let NewLine = lineaCopia.cloneNode(true);
+            NewLine.innerHTML = LetraArray[i];
+            Couplet.appendChild(NewLine);
+        }
+        
+    return LetraArray.slice(i+1,LetraArray.lenght);
+};
+
---
+++ js/validate_form.js
@@ -0,0 +1,20 @@
+function Validate_Form() {
+
+    var almsj = "No se completron los campos de";
+
+    if (!document.consulta.Nombre.value)
+        almsj += " Nombre";
+    if (!document.consulta.Apellido.value)
+        almsj += " Apellido";
+    if (!document.consulta.Mail.value)
+        almsj += " Mail";
+    if (!(document.consulta.Queja.checked || document.consulta.Consulta.checked || (document.consulta.Otro.checked && document.consulta.Otro_Texto.value)))
+        almsj += " Motivo";
+    if (!document.consulta.Mensaje.value)
+        almsj += " Mensaje";
+
+    if (almsj != "No se completron los campos de") {
+        alert(almsj);
+        return false;
+    }
+}
