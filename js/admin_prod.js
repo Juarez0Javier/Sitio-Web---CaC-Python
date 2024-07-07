03892eca99a4dff61746a05f0779a8dc3cc561d1
JuarezJavier <juarezjavier018@gmail.com>
2024-07-07 14:37:03 -0300

    Comenzando a trabajar la parte de admin

---
+++ js/admin_prod.js
@@ -0,0 +1,66 @@
+/*let TituloArray = ["En la Ciudad de la Furia","The Scientist","Eleanor Rigby"];
+let ArtistaArray = ["Soda Stereo","Coldplay", "The Beatles"];
+
+let linea = document.querySelector("#Linea");
+let lineaCopia = linea.cloneNode(true);
+
+linea.remove();
+
+let couplet = document.querySelector("#Couplet");
+let coupletCopia = couplet.cloneNode(true);
+
+couplet.remove();
+
+let lyrics = document.querySelector("#Lyrics");
+
+
+let Titulo = TituloArray[CH];
+let Artista = ArtistaArray[CH];*/
+
+
+fetch("http://127.0.0.1:5000/api/products/all/")
+.then (response => response.json())
+.then(data =>
+    {
+        print(data)
+
+        /*let Letra = data.lyrics;
+
+        Letra = Letra.replaceAll("\n\n","\n");
+
+        let LetraArray = Letra.split("\n");
+
+        document.querySelector("#Tema").innerHTML = "'" + Titulo + "' por " + Artista;
+
+        Create_Couplet(3,LetraArray);*/
+
+    }
+)
+.catch(error=>console.log(error));
+
+/*function Create_Couplet(N,LetraArray)
+{
+    var i;
+    for(let i=0;i<N;i++)
+        {
+            let NewCouplet = coupletCopia.cloneNode(true);
+            lyrics.appendChild(NewCouplet);
+            let NewArray = Create_Line(LetraArray.indexOf(""),LetraArray,NewCouplet);
+            LetraArray.length = 0;         
+            LetraArray.push.apply(LetraArray, NewArray);
+        }
+    
+};
+
+function Create_Line(N, LetraArray, Couplet)
+{
+    var i;
+    for(i=0;i<N;i++)
+        {
+            let NewLine = lineaCopia.cloneNode(true);
+            NewLine.innerHTML = LetraArray[i];
+            Couplet.appendChild(NewLine);
+        }
+        
+    return LetraArray.slice(i+1,LetraArray.lenght);
+};*/
---
+++ js/fetchData.js
@@ -0,0 +1,17 @@
+function  fetchData(url, method, callback, data_request = null) {
+    const options = {
+        mode: 'no-cors',
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: data_request ? JSON.stringify(data_request) : null,  // Si hay datos, los convierte a JSON y los incluye en el cuerpo
+    };
+
+    fetch(url, options)
+    .then(response => response.json())
+    .then(data_response => {
+        callback(data_response);
+    })
+    .catch(error => console.log("Ocurrió un error! " + error));
+}
